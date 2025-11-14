e10067263b9cf7fe8b117f4aff09b7b841a326ba
aderamadhana <sanade2034@gmail.com>
2025-11-14 14:22:51 +0700

    penyesuaian navbar dan landing page

--- src/components/layouts/navbar.vue
+++ src/components/layouts/navbar.vue
@@ -23,8 +23,6 @@
           <li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
           <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
           <li class="nav-item"><a class="nav-link" href="#projects">Portfolio</a></li>
-          <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-          <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
           <!-- Dropdown bahasa (dummy) -->
           <li class="nav-item ms-lg-3">
             <div class="lang-toggle">
